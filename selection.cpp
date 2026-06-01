65615b29f088696eaaa7c5501aef8940d1c4f80b
CrisCL20 <csoriano_01@hotmail.com>
2026-05-31 23:11:26 -0400

    evaluation func. & roulette wheel selection implementation

--- selection.cpp
+++ selection.cpp
@@ -6,6 +6,41 @@ using namespace std;
 
 // Function to evaluate the fitness of each individual in the population
 void evaluatePop(vector<individual> &pop){
+    for (auto& ind: pop) {
+        // obj_period = (sum(CR_courses_period) - MEAN_credits_periods) ^ 2
+
+        // get total credits per period
+        const size_t total_periods = ind.courses.size();
+        std::vector<int> total_credits_per_period(total_periods);
+        for (int i = 0; i < total_periods; ++i) 
+            total_credits_per_period[i] = std::accumulate(
+                ind.courses[i].begin(), ind.courses[i].end(),
+                0, [] (int sum, int cid) { return sum + course_credits[cid]; }
+            );
+
+        // get mean credits of current programming
+        float MEAN_credits = std::accumulate(
+            total_credits_per_period.begin(),
+            total_credits_per_period.end(),
+            (float) 0.0
+        ) / total_periods;
+
+        ind.period_fitness.clear();
+        ind.period_fitness.resize(total_periods);
+
+        // set the variance of each period
+        std::transform(
+            total_credits_per_period.begin(), total_credits_per_period.end(),
+            ind.period_fitness.begin(), [MEAN_credits] (int val) {
+                float diff = val - MEAN_credits;
+                return powf(diff, (float) 2.0);
+            }
+        );
+
+        // final fitness is the total variance
+        ind.fitness[0] = std::accumulate(ind.period_fitness.begin(), ind.period_fitness.end(), (float) 0.0);
+        
+    }
 }
 
 // Function to sort individuals in ascending order of fitness
@@ -16,7 +51,49 @@ bool fitnessComparisonAsc(const individual &a, individual &b) {
 
 // Roulette selection without replacement
 individual selection(vector<individual> &old_pop){
+    size_t pop_size = old_pop.size();
+    
+    // calculate selection probabilities
+    std::vector<float> selection_probs(pop_size);
+    float total_fit = std::accumulate(
+        old_pop.begin(), old_pop.end(),
+        (float) 0.0, [] (float sum, const individual& ind) {
+            return sum + ind.fitness[0];
+        }
+    );
 
+    std::transform(
+        old_pop.begin(), old_pop.end(),
+        selection_probs.begin(), [&total_fit] (const individual& ind) {
+            return ind.fitness[0] / total_fit;
+        }
+    );
+
+    // for minimization, inverse the probabilities and normalize so that its sum is 1
+    std::transform(
+        selection_probs.begin(), selection_probs.end(),
+        selection_probs.begin(), [&pop_size] (float p) {
+            return (1 - p) / (pop_size - 1);
+        }
+    );
+
+    float r = getRandomProb();
+    float cum_prob = 0.0;
+
+    for (int i = 0; i < old_pop.size(); ++i) {
+        cum_prob += selection_probs[i];
+        if (cum_prob >= r) 
+            return old_pop[i];
+    }
+
+    /*
+    default: return individual with best fitness
+    @note: is this the best way to select an individual if the previous loop fails?
+    */
+    auto max_it = std::max_element(selection_probs.begin(), selection_probs.end());
+    size_t best = std::distance(selection_probs.begin(), max_it);
+
+    return old_pop[best];
 }
 
 // Function to generate a new population from the old population using selection, crossover, and mutation
