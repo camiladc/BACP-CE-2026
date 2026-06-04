34109d4f6c3bf9d026263c9b6d2f32b5d39bd272
Camila Diaz <camilamdiazc@gmail.com>
2026-06-04 18:37:53 -0400

    update max credits and crossover comment

--- crossover.cpp
+++ crossover.cpp
@@ -4,11 +4,7 @@
 using namespace std;
 
 // Crossover functions
-// Crossover 1:
-// Select the best N semester from both parent as the base of the child, 
-// then try to relocate the pending courses in the same semester as the other parent, 
-// if not possible, create new semesters for them. 
-// If a semester gets empty in the new child, remove it and move the following semesters one position up.
+// swap a random prereq tree between two parents, ensuring that all constraints are still satisfied
 vector<individual> crossover(individual &parent1, individual &parent2){
 
 }
